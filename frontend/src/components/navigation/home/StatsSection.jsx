c0940f79eac246e4819ba56d0f111aac09ff3772
ale <tomescualexandramirela@gmail.com>
2026-03-06 19:27:30 +0200

    feat(home): add stats section with primary goal card

---
+++ frontend/src/components/navigation/home/StatsSection.jsx
@@ -0,0 +1,12 @@
+import PrimaryGoalCard from "./PrimaryGoalCard"
+
+function StatsSection() {
+    return (
+        <div className="home-stats-section">
+            <h3>Stats</h3>
+            <PrimaryGoalCard />
+        </div>
+    )
+}
+
+export default StatsSection
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,15 @@
 import GreetingHeader from "../components/navigation/home/GreetingHeader"
+import StatsSection from "../components/navigation/home/StatsSection"
 
-import "./Home.css"
+import "../styles/pages/Home.css"
 
 function Home() {
     return (
         <div className="home-layout">
             <div className="home-content">
                 <GreetingHeader />
+
+                <StatsSection />
             </div>
         </div>
     )
