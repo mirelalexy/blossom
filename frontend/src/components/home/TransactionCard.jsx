0f8a6fea88d4a049271e7b298cb1e4e0e534dc54
ale <tomescualexandramirela@gmail.com>
2026-03-06 21:37:01 +0200

    feat(home): add transaction card component

---
+++ frontend/src/components/home/TransactionCard.jsx
@@ -0,0 +1,36 @@
+import Card from "../ui/Card";
+
+import "./TransactionCard.css"
+
+function TransactionCard({ category, icon, merchant, date, mood, type, amount, currency }) {
+    const moodEmojis = {
+        happy: "🤩",
+        calm: "😌",
+        anxious: "😰",
+        neutral: "😐",
+        sad: "☹️"
+    }
+
+    const emoji = moodEmojis[mood] || "";
+
+    return (
+        <Card 
+            className="transaction-card" 
+            title={category} 
+            icon={icon}
+        >
+            <div className="transaction-card-content">
+                <div className="transaction-basics">
+                    <span className="transaction-merchant">{merchant}</span>
+                    <span className="transaction-amount">{type === "Expense" ? "-" : "+"}{amount} {currency}</span>
+                </div>
+
+                <div className="transaction-details">
+                    <p>{date} • {emoji}</p>
+                </div>
+            </div>
+        </Card>
+    )
+}
+
+export default TransactionCard;
