bc7691e9bd1d04b157a639902705621a94c9f4c2
ale <tomescualexandramirela@gmail.com>
2026-03-09 14:34:24 +0200

    fix(add transaction): fix input tags inside RadioGroup component

--- frontend/src/components/forms/RadioGroup.jsx
+++ frontend/src/components/forms/RadioGroup.jsx
@@ -6,7 +6,7 @@ function RadioGroup({ label, name, options = [], value, onChange }) {
             <div className="radio-group">
                 {options.map((opt) => (
                     <label key={opt.value} className="radio-option">
-                        <input type="radio" name={name} value={opt.value} checked={value === opt.value} onChange={() => onChange(opt.value)}> </input>
+                        <input type="radio" name={name} value={opt.value} checked={value === opt.value} onChange={() => onChange(opt.value)} />
                         <span className="radio-label">{opt.label}</span>
                     </label>
                 ))}
