49146aca136c675dd3e836540d2394b4a6143ae1
ale <tomescualexandramirela@gmail.com>
2026-03-13 17:46:02 +0200

    feat(profile): add navigation to settings page

--- frontend/src/components/profile/ProfileHeader.jsx
+++ frontend/src/components/profile/ProfileHeader.jsx
@@ -1,15 +1,19 @@
+import { useNavigate } from "react-router-dom"
+
 import Button from "../ui/Button"
 import Icon from "../ui/Icon"
 
 import "../../styles/components/ProfileHeader.css"
 
 function ProfileHeader({ bannerSrc, avatarSrc, name, email }) {
+    const navigate = useNavigate()
+
     return (
         <div className="profile-header">
             <div className="profile-banner">
                 <img src={bannerSrc} alt="profile banner" />
-                <div className="settings-btn">
-                    <Icon name="settings" />
+                <div className="settings-btn" onClick={() => navigate("/settings")}>
+                    <Icon name="settings"/>
                 </div>
             </div>
 
