acdc8fc808cee1080f824821f556ceaeb6e721b2
ale <tomescualexandramirela@gmail.com>
2026-03-19 00:09:43 +0200

    fix(home): correct logic for night hours

--- frontend/src/utils/dateUtils.js
+++ frontend/src/utils/dateUtils.js
@@ -53,9 +53,9 @@ export function getNextMonthInfo() {
 export function getTimeOfDay() {
     const hour = new Date().getHours()
 
-    if (hour > 4 && hour < 12) return "morning"
-    if (hour < 18) return "afternoon"
-    if (hour < 21) return "evening"
+    if (hour >= 5 && hour < 12) return "morning"
+    if (hour >= 12 && hour < 18) return "afternoon"
+    if (hour >= 18 && hour < 21) return "evening"
 
     return "night"
 }
