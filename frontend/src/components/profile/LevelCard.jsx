b0e33f61da2747c40b8c7b42a240c92ca0608b22
ale <tomescualexandramirela@gmail.com>
2026-03-22 18:45:53 +0200

    feat(profile): make level card clickable to navigate to Journey page

--- frontend/src/components/profile/LevelCard.jsx
+++ frontend/src/components/profile/LevelCard.jsx
@@ -1,3 +1,5 @@
+import { useNavigate } from "react-router-dom"
+
 import Icon from "../ui/Icon"
 import Card from "../ui/Card"
 import ProgressBar from "../ui/ProgressBar"
@@ -5,15 +7,20 @@ import ProgressBar from "../ui/ProgressBar"
 import "../../styles/components/LevelCard.css"
 
 function LevelCard({ title, level, description, story, progress }) {
+    const navigate = useNavigate()
+
     return (
         <Card
             title="Current Stage"
             icon={<Icon name="categories" size={20} />}
             className="level-card-content"
+            onClick={() => navigate("/journey")}
         >
             <p className="level-title">{title} • Level {level}</p>
 
             <ProgressBar progress={progress}  />
+
+            <p className="level-subtitle">See and understand your journey.</p>
         </Card>
     )
 }
