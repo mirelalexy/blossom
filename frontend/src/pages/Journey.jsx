d3cf4ae675566d64774d003f50ac55eeb6e3b5b3
ale <tomescualexandramirela@gmail.com>
2026-03-22 18:54:15 +0200

    feat(journey): create Journey page layout

--- frontend/src/pages/Journey.jsx
+++ frontend/src/pages/Journey.jsx
@@ -1,5 +1,33 @@
+import PageHeader from "../components/ui/PageHeader"
+import NotificationItem from "../components/notifications/NotificationItem"
+import EmptyState from "../components/ui/EmptyState"
+import Section from "../components/ui/Section"
+
+import "../styles/pages/Journey.css"
+
 function Journey() {
-    return <h1>Journey</h1>
+
+    return (
+        <div className="journey-content">
+            <PageHeader title="Journey" />
+            
+            <Section title="Your Journey So Far">
+
+            </Section>
+            
+            <Section title="Your Patterns">
+
+            </Section>
+
+            <Section title="Statistics">
+
+            </Section>
+
+            <Section title="Insights">
+
+            </Section>
+        </div>
+    )
 }
 
 export default Journey
