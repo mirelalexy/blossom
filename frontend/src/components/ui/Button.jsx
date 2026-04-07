e4a3a31d272f0467bbee56a3f391e41a051cd5b0
ale <tomescualexandramirela@gmail.com>
2026-04-07 10:18:46 +0300

    fix(ui): fix className undefined bug for button and add disabled state

--- frontend/src/components/ui/Button.jsx
+++ frontend/src/components/ui/Button.jsx
@@ -1,11 +1,12 @@
 import "../../styles/components/Button.css"
 
-function Button({ icon, children, onClick, type="button", className }) {
+function Button({ icon, children, onClick, type="button", className, disabled }) {
     return (
         <button
             type={type}
             onClick={onClick}
-            className={`button ${className}`}
+            disabled={disabled}
+            className={`button ${className || ""}`}
         >
             {icon && <div className="button-icon">
                     {icon}
