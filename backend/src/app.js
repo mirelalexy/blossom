68d8fa5a4220447edf7bc05d9da8d94e9c28f3de
ale <tomescualexandramirela@gmail.com>
2026-04-07 12:15:26 +0300

    chore(backend): configure connection to database

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,8 @@
 import express from "express"
 import cors from "cors"
 
+import pool from "./db.js"
+
 const app = express()
 
 app.use(cors())
@@ -9,5 +11,9 @@ app.use(express.json())
 app.get("/", (req, res) => {
     res.send("API is running")
 })
+
+pool.query("SELECT NOW()")
+    .then(res => console.log("DB connected:", res.rows[0]))
+    .catch(err => console.log("DB error:", err))
 
 export default app
